9f80aa4a8d46ccc7e566c79d0e97508d9fe23a0c
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-25 21:07:35 -0700

    add box showing which time of day it is on daylight view

--- app/assets/javascripts/daylight.js
+++ app/assets/javascripts/daylight.js
@@ -2,4 +2,11 @@
 function daylight(index) {
   var dayColors = ["yellow", "orange", "black"];
   document.getElementById("body").style.background = dayColors[index];
+  if (index == 0) {
+    document.getElementById("day").innerHTML = "Morning"
+  } else if (index == 1) {
+    document.getElementById("day").innerHTML = "Afternoon"
+  } else if (index == 2) {
+    document.getElementById("day").innerHTML = "Evening"
+  }
 }
