86cd992672ead08fc806a20a9cef2fba536b264c
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-25 21:16:36 -0700

    refactor daylight app

--- app/assets/javascripts/daylight.js
+++ app/assets/javascripts/daylight.js
@@ -1,12 +1,10 @@
 
-function daylight(index) {
-  var dayColors = ["yellow", "orange", "black"];
-  document.getElementById("body").style.background = dayColors[index];
-  if (index == 0) {
-    document.getElementById("day").innerHTML = "Morning"
-  } else if (index == 1) {
-    document.getElementById("day").innerHTML = "Afternoon"
-  } else if (index == 2) {
-    document.getElementById("day").innerHTML = "Evening"
-  }
+function daylight(color,element) {
+  document.body.style.background = color;
+  status(element.innerHTML);
+  // console.log(element.innerHTML)
+}
+
+function status(day) {
+  document.getElementById("day").innerHTML = day;
 }
