1a72c658ab575d37759023ee539e5fce5a322d4f
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-25 18:59:45 -0700

    ediyt jackhammer so box changes color each click

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,8 +1,11 @@
 var counter = 0
+var colors = ["red", "orange", "yellow", "green", "purple"]
 
-function jackhammer() {
+
+function jackhammer(element) {
   counter++;
   if (counter === 5) {
     alert("You win!");
   }
+  element.style.background = colors[counter-1];
 }
